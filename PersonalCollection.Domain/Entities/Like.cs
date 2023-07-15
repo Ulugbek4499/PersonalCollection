413e026fc61d70d9b5d774b18623dfa5a44486ab
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-15 18:18:49 +0500

    save

---
+++ PersonalCollection.Domain/Entities/Like.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalCollection.Domain.Entities
+{
+    internal class Like
+    {
+    }
+}
--- PersonalCollection.Domain/Entities/Tag.cs
+++ PersonalCollection.Domain/Entities/Tag.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PersonalCollection.Domain.Commons;
 
 namespace PersonalCollection.Domain.Entities
 {
-    internal class Tag
+    public class Tag : BaseAuditableEntity
     {
+        public string? Name { get; set; }
+
+        public virtual ICollection<Item>? Items { get; set; }
     }
 }
