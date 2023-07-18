d0532bb34aa0a50930143d764e92b2566078bc36
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-18 16:52:28 +0500

    save

---
+++ PersonalCollection.Application/UseCases/Comments/Commands/CreateComment/CreateCommentCommand.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using MediatR;
+using PersonalCollection.Application.Commons.Exceptions;
+using PersonalCollection.Application.Commons.Interfaces;
+using PersonalCollection.Application.Commons.Models;
+using PersonalCollection.Domain.Entities;
+
+namespace PersonalCollection.Application.UseCases.Comments.Commands.CreateCommand
+{
+    public class CreateCommentCommand:IRequest<CommentDto>
+    {
+        public string? Content { get; set; }
+        public Guid ItemId { get; set; }
+    }
+
+    public class CreateCommandCommandHandler : IRequestHandler<CreateCommentCommand, CommentDto>
+    {
+
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CreateCommandCommandHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<CommentDto> Handle(CreateCommentCommand request, CancellationToken cancellationToken)
+        {
+            Item maybeItem =
+               _context.Items.SingleOrDefault(p => p.Id.Equals(request.ItemId));
+
+            ValidateItemIsNotNull(request, maybeItem);
+
+            var comment = new Comment()
+            {
+                Content = request.Content,
+                Item = maybeItem
+            };
+
+            comment = _context.Comments.Add(comment).Entity;
+            await _context.SaveChangesAsync(cancellationToken);\
+            
+            return _mapper.Map<CommentDto>(comment);
+        }
+
+        private void ValidateItemIsNotNull(CreateCommentCommand request, Item? maybeItem)
+        {
+            if (maybeItem == null)
+            {
+                throw new NotFoundException(nameof(Item), request.ItemId);
+            }
+        }
+    }
+}
---
+++ PersonalCollection.Application/UseCases/Comments/Commands/DeleteComment/DeleteCommentCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using PersonalCollection.Application.Commons.Exceptions;
+using PersonalCollection.Application.Commons.Interfaces;
+using PersonalCollection.Application.Commons.Models;
+
+namespace PersonalCollection.Application.UseCases.Comments.Commands.DeleteCommand
+{
+    public record DeleteCollectionCommand(Guid collectionId) : IRequest<CollectionDto>;
+
+    public class DeleteCollectionCommandHandler : IRequestHandler<DeleteCollectionCommand, CollectionDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly ICurrentUserService _currentUserService;
+
+        public DeleteCollectionCommandHandler(IApplicationDbContext context, IMapper mapper, ICurrentUserService currentUserService)
+        {
+            _context = context;
+            _mapper = mapper;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task<CollectionDto> Handle(DeleteCollectionCommand request, CancellationToken cancellationToken)
+        {
+
+            Collection maybeCollection = await _context.Collections.FindAsync(new object[] { request.collectionId });
+
+            ValidateDepartmentIsNotNull(request, maybeCollection);
+
+            // Check if the CreatedBy field of the collection matches the Id of the current user
+            if (maybeCollection.CreatedBy != _currentUserService.Id)
+            {
+                throw new UnauthorizedException("User could not delete this collection.");
+            }
+
+            _context.Collections.Remove(maybeCollection);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return _mapper.Map<CollectionDto>(maybeCollection);
+        }
+
+        private static void ValidateDepartmentIsNotNull(DeleteCollectionCommand request, Collection maybeCollection)
+        {
+            if (maybeCollection is null)
+            {
+                throw new NotFoundException(nameof(Collection), request.collectionId);
+            }
+        }
+    }
+}
