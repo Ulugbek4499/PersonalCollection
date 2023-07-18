a73cf701f91a5d629931a1b52a3d4abc0ebd527e
Ulugbek4499 <ulugbektoshtemirov2000@gmail.com>
2023-07-18 06:51:25 +0500

    save

--- PersonalCollection.Application/Commons/Models/CommentDto.cs
+++ PersonalCollection.Application/Commons/Models/CommentDto.cs
@@ -14,7 +14,5 @@ namespace PersonalCollection.Application.Commons.Models
         public ItemDto? Item { get; set; }
         public DateTime Created { get; set; }
         public string? CreatedBy { get; set; }
-        public DateTime? LastModified { get; set; }
-        public string? LastModifiedBy { get; set; }
     }
 }
